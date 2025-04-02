98e9bef33a54b79b74fae70ee42d2710af55a458
gabrielesilv <122821654+gabrielesilv@users.noreply.github.com>
2025-04-02 12:30:12 -0300

    inclusão da resolução desafio

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,15 +2,17 @@ package main.java;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CorpoHumano c1 = new CorpoHumano(80, 10);
+        //CorpoHumano c1 = new CorpoHumano(80, 10); //construtor para atribuir valores aos atributos
+        //System.out.println(c1.getDensidade());
         
         //c1.massa = "2";
 
-        CorpoHumano c2 = new CorpoHumano(70, 10);
-
-        System.out.println(c1.getDensidade());
+        CorpoHumano c2 = new CorpoHumano(70, 10); 
         System.out.println(c2.getDensidade());
 
+
+        c2.CalcularImc(1.60, 55);
+        System.out.println(c2.getImc());
 
     }
 }
--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -4,11 +4,20 @@ public class CorpoHumano {
     private float massa;
     private float volume;
     private float densidade; //não tem set pois vai ser utilizada apenas pra recuperar o valor da operação
+    private double altura;
+    private int peso;
+    private double imc;
 
-    public CorpoHumano(float m, float v){
+    public CorpoHumano(float m, float v) {
         this.massa = m;
         this.volume = v;
-        densidade = m / v;
+        densidade = m/v;
+    }
+
+    public void CalcularImc(double a, int p){
+        this.altura = a;
+        this.peso = p;
+        imc = (a*a)/p;
     }
 
     public float getMassa() {
@@ -22,4 +31,17 @@ public class CorpoHumano {
     public float getDensidade() {
         return this.densidade;
     }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public double getImc() {
+        return imc;
+    }
+
 }
